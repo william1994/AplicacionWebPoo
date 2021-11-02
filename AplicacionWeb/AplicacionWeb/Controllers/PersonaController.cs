168193512b0d04b29377182598113a2b651e0e46
William Diaz <williamefrain1994@gmail.com>
2021-11-02 10:00:24 -0600

    GO

---
+++ AplicacionWeb/AplicacionWeb/Controllers/PersonaController.cs
@@ -0,0 +1,63 @@
+﻿using AplicacionWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionWeb.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult PersonaVista() {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonaVista(Persona p)
+        {
+
+            Console.WriteLine(p.Nombre);
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult ClaseHtml() {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ClaseHtml(Persona p)
+        {
+            _ = p;
+
+            if (p.Nombre.Equals("William") && p.Contr.Equals("123")) {
+
+                return Redirect("Welcome");
+            }
+        
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Welcome() {
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ AplicacionWeb/AplicacionWeb/Models/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionWeb.Models
+{
+    public class Persona
+    {
+        public String Nombre { get; set; }
+
+        public String Apellido { get; set; }
+
+        public String Contr { get; set; }
+
+        
+
+
+    }
+}
